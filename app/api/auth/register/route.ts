f70103cb7dd24931055772f096f6deb15b3daf00
Piyumal-Bandaranayake <piyumalbandaranayake24790@gmail.com>
2026-02-10 11:04:31 +0530

    feat: Implement user authentication (login/register) and basic task management APIs with Prisma and JWT.

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  try {
+    const { email, password } = await req.json();
+
+    
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
